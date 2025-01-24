fcd0840fe8844f1473bddf3836fa0d99a8801fc2
skx <3244104494>
2025-01-24 20:18:47 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,12 +9,11 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +21,7 @@ import java.util.Map;
 /**
  * 员工管理
  */
+@Api(tags = "员工相关接口")
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
@@ -37,8 +38,10 @@ public class EmployeeController {
      * @param employeeLoginDTO
      * @return
      */
+
+    @ApiOperation(value = "员工登录")
     @PostMapping("/login")
-    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {  // 这个DTO 表示登录时提交的数据
         log.info("员工登录：{}", employeeLoginDTO);
 
         Employee employee = employeeService.login(employeeLoginDTO);
@@ -66,9 +69,20 @@ public class EmployeeController {
      *
      * @return
      */
+    @ApiOperation(value = "员工退出")
     @PostMapping("/logout")
     public Result<String> logout() {
         return Result.success();
     }
+
+
+    @ApiOperation(value = "新增员工")
+    @PostMapping
+    public Result<String> save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工{}",employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success("新增成功");
+    }
+
 
 }
